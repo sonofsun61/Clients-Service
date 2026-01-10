4333438fca678a7e92988270adc724151d136a05
Igor <igoryanec23@gmail.com>
2026-01-10 18:41:45 +0300

    Add handlers

--- internal/transport/handler.go
+++ internal/transport/handler.go
@@ -1,16 +1,15 @@
 package transport
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/AI-Hackathon-2026/Clients-Service/internal/model"
 )
 
 type service interface {
-    findProfile(username string) (model.GetUserProfilePayload, error)
-    updateProfile(username string) (model.UpdateUserProfilePayload, error)
-    findGraph(graphId string) (model.GraphPayload, error)
+    findProfile(username string) (*model.GetUserProfilePayload, error)
+    updateProfile(*model.UpdateUserProfilePayload) error
+    findGraphs( string) ([]model.GraphPayload, error)
 }
 
 type Handler struct {
@@ -25,7 +24,7 @@ func NewHandler(srv service) *Handler {
 
 func (h *Handler) RegisterRoutes(router *http.ServeMux) {
     router.HandleFunc("GET /profile-edit/{username}", h.getProfile)
-    router.HandleFunc("PUT /profile-edit/{username}", h.putProfile)
+    router.HandleFunc("PUT /profile-edit/", h.putProfile)
 
     router.HandleFunc("GET /graphs/{username}", h.getGraphs)
 }
@@ -34,17 +33,31 @@ func (h *Handler) getProfile(w http.ResponseWriter, r *http.Request) {
     username := r.PathValue("username")
     prfl, err := h.srv.findProfile(username)
     if err != nil {
-        http.Error(w, "Something went wrong", http.StatusBadRequest)
-        log.Printf("getProfile error: %v", err)
+        raiseError(w, "getProfile error:", err)
         return
     }
-    
+    WriteJson(w, http.StatusOK, prfl)     
 }
 
 func (h *Handler) putProfile(w http.ResponseWriter, r *http.Request) {
-
+    prfl := new(model.UpdateUserProfilePayload)
+    if err := ParseJson(r, prfl); err != nil {
+        raiseError(w, "putProfile, ParseJson error:", err)
+        return
+    }
+    if err := h.srv.updateProfile(prfl); err != nil {
+        raiseError(w, "putProfile, updateProfile error:", err)
+        return
+    }
 }
 
 func (h *Handler) getGraphs(w http.ResponseWriter, r *http.Request) {
-
+    userId := r.PathValue("username")    
+    graphs, err := h.srv.findGraphs(userId)
+    if err != nil {
+        raiseError(w, "putProfile, updateProfile error:", err)
+        return
+    }
+    WriteJson(w, http.StatusOK, graphs)
 }
+
