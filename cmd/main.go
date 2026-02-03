f1f00753331f23ff40b517bb1093510df222a34b
Igor <igoryanec23@gmail.com>
2026-02-03 17:17:41 +0300

    Add environment variable compatibility

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/joho/godotenv"
 
 	"github.com/AI-Hackathon-2026/Clients-Service/internal/app"
-	"github.com/DATA-DOG/go-sqlmock"
+)
+
+const (
+    defaultPort = ":9191"
 )
 
 func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    port, exists := os.LookupEnv("PORT")
+    if !exists {
+        port = defaultPort
+    }
+
     db, mock, err := sqlmock.New()
     if err != nil {
         log.Fatal(err)
     }
 
-    apl := app.NewApp(":9191", db, mock)
+    apl := app.NewApp(port, db, mock)
     apl.Run()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AI-Hackathon-2026/Clients-Service
 
 go 1.25.3
 
-require github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/joho/godotenv v1.5.1
+)
