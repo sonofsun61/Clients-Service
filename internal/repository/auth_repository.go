d1e01dbc1ddaf139220f12bf407a9f35721ff12f
Gleb <glebtarn@gmail.com>
2026-03-26 15:10:21 +0300

    fix: comment methods to real db to mock methods work correctly

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -3,8 +3,6 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"time"
 
 	"github.com/AI-Hackathon-2026/Clients-Service/internal/model"
 )
@@ -27,40 +25,36 @@ func NewAuthRepository(db *sql.DB, mocker *Mocker) AuthRepository {
 }
 
 func (r *authRepository) Register(ctx context.Context, userData model.UserData) error {
-	if r.db == nil {
-		return r.mocker.MockRegister(userData)
-	}
-	query := `
-		INSERT INTO users (user_id, username, password_hash, streak_start, last_activity)
-		VALUES ($1, $2, $3, $4, $5)
-	`
-	_, err := r.db.ExecContext(ctx, query, userData.UserID, userData.Username, userData.PasswordHash, time.Now(), time.Now())
-	if err != nil {
-		return fmt.Errorf("failed to insert user: %w", err)
-	}
-	return nil
+	return r.mocker.MockRegister(userData)
+	// query := `
+	// 	INSERT INTO users (user_id, username, password_hash, streak_start, last_activity)
+	// 	VALUES ($1, $2, $3, $4, $5)
+	// `
+	// _, err := r.db.ExecContext(ctx, query, userData.UserID, userData.Username, userData.PasswordHash, time.Now(), time.Now())
+	// if err != nil {
+	// 	return fmt.Errorf("failed to insert user: %w", err)
+	// }
+	// return nil
 }
 
 func (r *authRepository) GetUserByEmail(ctx context.Context, reqEmail string) (model.UserData, error) {
-	if r.db == nil {
-		return r.mocker.MockLogin(reqEmail)
-	}
-	query := `
-		SELECT user_id, username, email, password_hash, streak_start, last_activity
-		FROM users
-		WHERE email = $1
-	`
-	var userData model.UserData
-	err := r.db.QueryRowContext(ctx, query, reqEmail).Scan(
-		&userData.UserID,
-		&userData.Username,
-		&userData.Email,
-		&userData.PasswordHash,
-		&userData.StreakStart,
-		&userData.LastActivity,
-	)
-	if err != nil {
-		return model.UserData{}, fmt.Errorf("query failed: %w", err)
-	}
-	return userData, nil
+	return r.mocker.MockLogin(reqEmail)
+	// query := `
+	// 	SELECT user_id, username, email, password_hash, streak_start, last_activity
+	// 	FROM users
+	// 	WHERE email = $1
+	// `
+	// var userData model.UserData
+	// err := r.db.QueryRowContext(ctx, query, reqEmail).Scan(
+	// 	&userData.UserID,
+	// 	&userData.Username,
+	// 	&userData.Email,
+	// 	&userData.PasswordHash,
+	// 	&userData.StreakStart,
+	// 	&userData.LastActivity,
+	// )
+	// if err != nil {
+	// 	return model.UserData{}, fmt.Errorf("query failed: %w", err)
+	// }
+	// return userData, nil
 }
